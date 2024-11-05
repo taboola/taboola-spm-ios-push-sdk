5d44d518356db8c2e10adaa0b626baeafabe7715
BojanKarlicic <bojan.karlicic@taboola.com>
2024-11-05 19:25:37 +0100

    Updated SDK upload

---
+++ TaboolaPush.xcframework/ios-arm64_x86_64-simulator/TaboolaPush.framework/Headers/TaboolaPush.h
@@ -0,0 +1,18 @@
+//
+//  TaboolaPush.h
+//  TaboolaPush
+//
+//  Created by Bojan Karlicic on 6.10.24..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TaboolaPush.
+FOUNDATION_EXPORT double TaboolaPushVersionNumber;
+
+//! Project version string for TaboolaPush.
+FOUNDATION_EXPORT const unsigned char TaboolaPushVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TaboolaPush/PublicHeader.h>
+
+
